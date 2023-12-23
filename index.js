497da8c84b2d16bf6737fee05ab021c22d373166
Cuitlahuatl <cmorenomagana@gmail.com>
2023-12-22 18:19:37 -0600

    Sending Data With Fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    };
+ return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (repsonse) {
+        return repsonse.json();
+    })
+    .then(function (data) {
+        console.log(data)
+      document.body.innerHTML = data.id
+    })
+
+    .catch(function (error) {
+        alert("user failed");
+        console.log(error.message)
+        document.body.innerHTML = error.message
+    })
+};
